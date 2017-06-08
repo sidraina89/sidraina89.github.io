e9c9fa6b2e376e7f511cd08f129884792d384e01
Siddharth Raina <sidraina89@gmail.com>
2017-06-08 18:08:51 -0500

    Created index and about pages

---
+++ build_site.R
@@ -0,0 +1,9 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("C:\\Users\\sraina\\Documents\\personal_site\\sidraina89.github.io")
+
+
+#render your sweet site. 
+rmarkdown::render_site()
